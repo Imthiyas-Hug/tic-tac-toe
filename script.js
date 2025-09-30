a05a43104dc17f5eb239595179e64befd4198547
Imthiyas-Hug <imthiyashugi@gmail.com>
2025-09-30 17:29:50 +0530

    Modify files

--- script.js
+++ script.js
@@ -1,30 +1,89 @@
-const p1 = window.prompt('Player1: Enter X or O').toLowerCase();
-let p2;
-let playFirst = 'x';
-let playSecond = 'o';
-let mark = '';
+const player1_vs_player2_btn = document.querySelector('.player1-vs-player2');
+const player_vs_computer_btn = document.querySelector('.player-vs-computer');
+const board_buttons = document.querySelectorAll('.container button');
+const first_row = document.querySelectorAll('.first-row button');
+const second_row = document.querySelectorAll('.second-row button');
+const third_row = document.querySelectorAll('.third-row button');
 
-const p1Choice = document.querySelector('.player1-choice')
-p1Choice.textContent = p1;
+let playerChoice;
+let computerChoice;
+player_vs_computer_btn.addEventListener('click', () => {
+    playerChoice = window.prompt("Choose 'X' or 'O' ").toUpperCase();
+    if (playerChoice == 'X') {
 
-const p2Choice = document.querySelector('.player2-choice')
-p2Choice.textContent = p2;
+        computerChoice = 'O';
+    }
+    else {
 
-const boardButtons = document.querySelectorAll('.container button');
-boardButtons.forEach(button => {
-   
-    button.addEventListener('click', () => {
-        if(mark == ''){
-            button.textContent = 'x';
-            mark = 'o';
+        computerChoice = 'X';
+    }
+})
+
+const array = [
+    ['', '', ''],
+
+    ['', '', ''],
+
+    ['', '', '']
+];
+
+let random_row;
+let random_col;
+
+first_row.forEach(button => {
+    button.addEventListener('click', (event) => {
+        random_row = Math.floor(Math.random() * 3);
+        random_col = Math.floor(Math.random() * 3);
+        array[0][event.target.dataset.index] = playerChoice;
+        button.textContent = playerChoice;
+        for (let i = 0; i < 3; i++) {
+            if (random_col == event.target.dataset.index) {
+                continue;
+            }
+            random_col = i;
         }
-        else if(mark == 'o'){
-            button.textContent = 'o';
-            mark = 'x';
-        }
-        else if(mark == 'x'){
-            button.textContent = 'x';
-            mark = 'o';
-        }
+        if(!(array[random_row][random_col]))
+            array[random_row][random_col] = computerChoice;
+            console.log(array);
+        })
+})
+
+second_row.forEach(button => {
+    button.addEventListener('click', (event) => {
+        random_row = Math.floor(Math.random() * 3);
+        random_col = Math.floor(Math.random() * 3);
+        array[1][event.target.dataset.index] = playerChoice;
+        button.textContent = playerChoice;
+        array[random_row][random_col] = computerChoice;
+        console.log(array)
     })
 })
+
+third_row.forEach(button => {
+    button.addEventListener('click', (event) => {
+        random_row = Math.floor(Math.random() * 3);
+        random_col = Math.floor(Math.random() * 3);
+        array[2][event.target.dataset.index] = playerChoice;
+        button.textContent = playerChoice;
+        array[random_row][random_col] = computerChoice;
+        console.log(array)
+    })
+})
+console.log(board_buttons)
+// board_buttons.forEach(button => {
+//     button.addEventListener('click', (event) => {
+//         if (random_col == event.target.dataset.index) {
+// for (let i = 0; i < 3; i++) {
+//     if (random_col == i) {
+//         continue;
+//     }
+//                 random_col = i;
+//                 array[random_row][random_col] = computerChoice;
+//                 button.textContent = array[random_row][random_col];
+//             }
+//         }
+
+
+//     })
+// })
+
