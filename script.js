36c5100b42816148bf6c87bb085a590074197359
Imthiyas-Hug <imthiyashugi@gmail.com>
2025-10-03 17:31:42 +0530

    Modify files

--- script.js
+++ script.js
@@ -6,14 +6,162 @@ const gameBoard = (function () {
         ['', '', ''],
     ];
 
-    function mark(row, col, value, whoseTurn) {
-        array[row][col] = value;
-        console.log(array);
-    
-        if (whoseTurn == 'Player 1') {
-            getIndices('Player 2');
-        } else {
-            getIndices('Player 1');
+    let emptyArray = [
+        ['', '', ''],
+        ['', '', ''],
+        ['', '', ''],
+    ];
+
+    let leftDiagonal;
+    let rightDiagonal;
+
+    let firstColumn = [];
+    let secondColumn = [];
+    let thirdColumn = [];
+
+    let p1FirstRowCheck;
+    let p1SecondRowCheck;
+    let p1ThirdRowCheck;
+
+    let p2FirstRowCheck;
+    let p2SecondRowCheck;
+    let p2ThirdRowCheck;
+
+    let p1FirstColCheck;
+    let p1SecondColCheck;
+    let p1ThirdColCheck;
+
+    let p2FirstColCheck;
+    let p2SecondColCheck;
+    let p2ThirdColCheck;
+
+    let p1LeftDiagonalCheck;
+    let p1RightDiagonalCheck;
+
+    let p2LeftDiagonalCheck;
+    let p2RightDiagonalCheck;
+
+    let player1Winner;
+    let player2Winner;
+
+    function arrayNotFull() {
+        return array.some(row => row.some(cell => cell == ''));
+    }
+
+    function mark(row, col, value, whoseTurn, player1Choice, opponentChoice, whoVersusWho) {
+
+        //assigning value
+        if (row && col) {
+            if (array[row][col] == '') {
+                array[row][col] = value;
+                console.table(array);
+            }
+            else if (whoseTurn = 'Player 1' || whoseTurn == 'Player 2') {
+                alert('Spot taken Already! Mark Again.');
+                getIndices(whoseTurn);
+            }
+            else if (whoseTurn = 'Computer') {
+                getIndices(whoseTurn);
+            }
+        }
+
+        //checking rows for player1 value 
+        p1FirstRowCheck = array[0].every(cell => cell == player1Choice);
+        p1SecondRowCheck = array[1].every(cell => cell == player1Choice);
+        p1ThirdRowCheck = array[2].every(cell => cell == player1Choice);
+
+        //checking rows for player2/computer value 
+        p2FirstRowCheck = array[0].every(cell => cell == opponentChoice);
+        p2SecondRowCheck = array[1].every(cell => cell == opponentChoice);
+        p2ThirdRowCheck = array[2].every(cell => cell == opponentChoice);
+
+        //extracting each column value and assigning
+        for (let i = 0; i < 3; i++) {
+            for (let j = 0; j < 1; j++) {
+                firstColumn[i] = array[i][j];
+            }
+        }
+        for (let i = 0; i < 3; i++) {
+            for (let j = 1; j < 2; j++) {
+                secondColumn[i] = array[i][j];
+            }
+        }
+        for (let i = 0; i < 3; i++) {
+            for (let j = 2; j < 3; j++) {
+                thirdColumn[i] = array[i][j];
+            }
+        }
+
+        //checking columns for player1 value 
+        p1FirstColCheck = firstColumn.every(cell => cell == player1Choice);
+        p1SecondColCheck = secondColumn.every(cell => cell == player1Choice);
+        p1ThirdColCheck = thirdColumn.every(cell => cell == player1Choice);
+
+        //checking columns for player2/computer value 
+        p2FirstColCheck = firstColumn.every(cell => cell == opponentChoice);
+        p2SecondColCheck = secondColumn.every(cell => cell == opponentChoice);
+        p2ThirdColCheck = thirdColumn.every(cell => cell == opponentChoice);
+
+        //assigning diagonal values
+        leftDiagonal = [array[0][0], array[1][1], array[2][2]];
+        rightDiagonal = [array[0][2], array[1][1], array[2][0]];
+
+        //checking diagonal for player1 value
+        p1LeftDiagonalCheck = leftDiagonal.every(cell => cell == player1Choice);
+        p1RightDiagonalCheck = rightDiagonal.every(cell => cell == player1Choice);
+
+        //checking diagonal for player2/computer value
+        p2LeftDiagonalCheck = leftDiagonal.every(cell => cell == opponentChoice);
+        p2RightDiagonalCheck = rightDiagonal.every(cell => cell == opponentChoice);
+
+        //checking player1 result
+        player1Winner = (p1FirstRowCheck || p1SecondRowCheck || p1ThirdRowCheck)
+            || (p1FirstColCheck || p1SecondColCheck || p1ThirdColCheck)
+            || (p1LeftDiagonalCheck || p1RightDiagonalCheck) ? true : false;
+
+        //checking player2/computer result
+        player2Winner = ((p2FirstRowCheck || p2SecondRowCheck || p2ThirdRowCheck)
+            || (p2FirstColCheck || p2SecondColCheck || p2ThirdColCheck)
+            || (p2LeftDiagonalCheck || p2RightDiagonalCheck)) ? true : false;
+
+        //alerting player1 result
+        if (player1Winner) {
+            alert('Player 1 wins!');
+            console.log('Player 1 wins!');
+        }
+
+        //alerting player2 result
+        if (player2Winner && whoseTurn == 'Player 2') {
+            alert('Player 2 wins!');
+            console.log('Player 2 wins!');
+        }
+
+        //alerting computer result
+        if (player2Winner && whoseTurn == 'Computer') {
+            alert('Computer wins!');
+            console.log('Computer wins!');
+        }
+
+        //getting row and col values for each turn 
+        if (arrayNotFull() && (!player1Winner) && (!player2Winner)) {
+            if (whoseTurn == 'Player 1') {
+                if (whoVersusWho == '1') {
+                    getIndices('Player 2');
+                }
+                else {
+                    getIndices('Computer')
+                }
+            }
+            else if (whoseTurn == 'Player 2') {
+                getIndices('Player 1');
+            }
+
+        }
+
+        //checking for match tie
+        if ((!arrayNotFull()) && (!player1Winner) && (!player2Winner)) {
+            alert('Match Tie!');
+            console.log('Match Tie!');
         }
     }
 
@@ -23,35 +171,72 @@ const gameBoard = (function () {
 
 })();
 
-console.log('__00__|__01__|__02__');
-console.log('__10__|__11__|__12__');
-console.log('  20  |  21  |  22  ');
-
 let player1Choice;
 let player2Choice;
+let computerChoice;
 let playsFirst;
 let row;
 let col;
 let whoseTurn;
 
-getChoices();
+alert('Choose who do you want to play with!');
+let whoVersusWho = window.prompt('Computer(0) or Player(1)');
+
+const input = ['0', '1', '2'];
+const marker = ['X', 'O'];
+
 function getChoices() {
-    player1Choice = window.prompt('Enter Player 1 Choice: "X" or "O" ').toUpperCase();
-    player2Choice = (player1Choice == "X") ? "O" : "X";
-    window.alert(`Player 2 Choice is "${player2Choice}"`);
-    playsFirst = (player1Choice == "X") ? 'Player 1' : 'Player 2';
-    window.alert(`X plays first!`);
-    whoseTurn = playsFirst;
-    getIndices(whoseTurn);
+    if (whoVersusWho == '1') {
+        player1Choice = window.prompt('Enter Player 1 Choice: "X" or "O" ').toUpperCase();
+        if (!(marker.includes(player1Choice))) {
+            alert('Invalid Choice! Enter Again.')
+            getChoices();
+        }
+        player2Choice = (player1Choice == "X") ? "O" : "X";
+        window.alert(`Player 2 Choice is "${player2Choice}"`);
+        playsFirst = (player1Choice == "X") ? 'Player 1' : 'Player 2';
+        window.alert(`X plays first!`);
+        whoseTurn = playsFirst;
+        getIndices(whoseTurn);
+    }
+    else {
+        player1Choice = window.prompt('Enter Player 1 Choice: "X" or "O" ').toUpperCase();
+        if (!(marker.includes(player1Choice))) {
+            alert('Invalid Choice! Enter Again.')
+            getChoices();
+        }
+        computerChoice = (player1Choice == "X") ? "O" : "X";
+        window.alert(`Computer Choice is "${computerChoice}"`);
+        playsFirst = (player1Choice == "X") ? 'Player 1' : 'Computer';
+        window.alert(`X plays first!`);
+        whoseTurn = playsFirst;
+        getIndices(whoseTurn);
+    }
 }
+getChoices();
 
 function getIndices(whoseTurn) {
     window.alert(`${whoseTurn} Turn:`);
-    row = window.prompt(`(${whoseTurn} Turn) Enter the row number(0,1,2) you want to mark.`);
-    col = window.prompt(`(${whoseTurn} Turn) Enter the col number(0,1,2) you want to mark.`);
+    if (whoseTurn = 'Player 1' || whoseTurn == 'Player 2') {
+        row = window.prompt(`(${whoseTurn} Turn) Enter the row number(0,1,2) you want to mark.`);
+        col = window.prompt(`(${whoseTurn} Turn) Enter the col number(0,1,2) you want to mark.`);
+    }
+    if (whoseTurn == 'Computer') {
+        row = Math.floor(Math.random() * 3);
+        col = Math.floor(Math.random() * 3);
+    }
+    if (!(input.includes(row)) || !(input.includes(col))) {
+        alert('Invalid Input! Enter Again.');
+        getIndices(whoseTurn);
+    }
     if (whoseTurn == 'Player 1') {
-        gameBoard.mark(row, col, player1Choice, whoseTurn);
-    } else {
-        gameBoard.mark(row, col, player2Choice, whoseTurn);
+        gameBoard.mark(row, col, player1Choice, whoseTurn, player1Choice, player2Choice,whoVersusWho);
+    }
+    else if (whoseTurn == 'Player 2') {
+        gameBoard.mark(row, col, player2Choice, whoseTurn, player1Choice, player2Choice,whoVersusWho);
+    }
+    else if (whoseTurn == 'Computer') {
+        gameBoard.mark(row, col, computerChoice, whoseTurn, player1Choice, computerChoice, whoVersusWho);
     }
 }
+
