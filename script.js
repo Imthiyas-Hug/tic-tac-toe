a14c3c26775098916e644a91d7ed2b1ce93f6de2
Imthiyas-Hug <imthiyashugi@gmail.com>
2025-10-06 15:25:11 +0530

    Modify files

--- script.js
+++ script.js
@@ -7,13 +7,32 @@ let cellId;
 let whoseTurn;
 let whoVersusWho;
 let turn = false;
-const input = [0, 1, 2];
-// const marker = ["X", "O"];
+
 const cells = document.querySelectorAll(".cell");
-const playBtns = document.querySelectorAll(".play-buttons button");
+const playWithPcbtn = document.querySelector("#playWithComputer-btn");
+const playWithFrndbtn = document.querySelector("#playWithFrnd-btn");
 const markerBtns = document.querySelectorAll(".marker-btn button");
-const dialog = document.querySelector("dialog");
+const markerDialog = document.querySelector("#marker-dialog");
+const playerMarker = document.querySelector('#player-marker')
+const resultDialog = document.querySelector("#result-dialog");
+const playerNameDialog = document.querySelector("#playerName-dialog");
+const playersNameDialog = document.querySelector("#playersName-dialog");
+const playFirstDialog = document.querySelector("#playFirst-dialog");
+const result = document.querySelector("#result");
+const restartBtn = document.querySelector("#restart-btn");
+const playAgainbBtn = document.querySelector("#playAgain-btn");
+const playerNameSubmitBtn = document.querySelector("#playerNameSubmit-btn");
+const playersNameSubmitBtn = document.querySelector("#playersNameSubmit-btn");
 const board = document.querySelector('.board');
+const playerName = document.querySelector('#playerName');
+const player1Name = document.querySelector('#player1Name');
+const player2Name = document.querySelector('#player2Name');
+const player1Status = document.querySelector('.player1-status');
+const player2Status = document.querySelector('.player2-status');
+const p1Status = document.querySelector('#p1Status');
+const p1Marker = document.querySelector('#p1Marker');
+const p2Status = document.querySelector('#p2Status');
+const p2Marker = document.querySelector('#p2Marker');
 
 const gameBoard = (function () {
 
@@ -28,13 +47,16 @@ const gameBoard = (function () {
             ['', '', ''],
             ['', '', ''],
             ['', '', ''],
-       ];
+        ];
         player1Winner = false;
         player2Winner = false;
-        cells.forEach(cell=>{
+        cells.forEach(cell => {
             cell.textContent = "";
         })
     }
+
+    let p1Name;
+    let p2Name;
 
     let leftDiagonal;
     let rightDiagonal;
@@ -75,7 +97,7 @@ const gameBoard = (function () {
     function mark(row, col, value, whoseTurn, player1Choice, opponentChoice, cellId) {
 
         //assigning value
-        if (arrayNotFull() && (!player1Winner) && (!player2Winner)) {   
+        if (arrayNotFull() && (!player1Winner) && (!player2Winner)) {
             if (array[row][col] == '') {
                 array[row][col] = value;
                 cells[cellId].textContent = array[row][col];
@@ -84,17 +106,16 @@ const gameBoard = (function () {
                 cells[cellId].classList.add('marked');
                 console.table(array);
             }
-            else{
-                if(whoseTurn == 'Computer') {
+            else {
+                if (whoseTurn == 'Computer') {
                     getComputerIndices(whoseTurn);
                 }
-                else{
-                    alert('Spot taken Already! Mark Again.');
+                else {
                     return;
                 }
-  
+
             }
-  
+
         }
 
         //checking rows for player1 value 
@@ -146,21 +167,30 @@ const gameBoard = (function () {
 
         //alerting player1 result
         if (player1Winner) {
-            alert('Player 1 wins!');
+            result.textContent = `${gameBoard.p1Name} Wins!`;
+            setTimeout(() => {
+                resultDialog.showModal();
+            }, 800);
             console.log('Player 1 wins!');
             return;
         }
 
         //alerting player2 result
         if (player2Winner && whoseTurn == 'Player 2') {
-            alert('Player 2 wins!');
+            result.textContent = `${gameBoard.p2Name} Wins!`;
+            setTimeout(() => {
+                resultDialog.showModal();
+            }, 800);
             console.log('Player 2 wins!');
             return;
         }
 
         //alerting computer result
         if (player2Winner && whoseTurn == 'Computer') {
-            alert('Computer wins!');
+            result.textContent = 'Computer wins!';
+            setTimeout(() => {
+                resultDialog.showModal();
+            }, 800);
             console.log('Computer wins!');
             return;
         }
@@ -169,18 +199,16 @@ const gameBoard = (function () {
         if (arrayNotFull() && (!player1Winner) && (!player2Winner)) {
             if (whoseTurn == 'Player 1') {
                 if (whoVersusWho == '1') {
-                    window.alert('Player 2 Turn:');
+                    // window.alert('Player 2 Turn:');
                 }
-                 else {
-                    // window.alert(`Computer Turn:`);
-                    setTimeout(()=>{
+                else {
+                    setTimeout(() => {
                         getComputerIndices('Computer');
-                    },1000);
-                    
-                 }
+                    }, 800);
+                }
             }
             else if (whoseTurn == 'Player 2') {
-                  window.alert('Player 1 Turn:');
+                // window.alert('Player 1 Turn:');
             }
             else if (whoseTurn == 'Computer') {
                 // window.alert('Player 1 Turn:');
@@ -189,14 +217,17 @@ const gameBoard = (function () {
 
         //checking for match tie
         if ((!arrayNotFull()) && (!player1Winner) && (!player2Winner)) {
-            alert('Match Tie!');
+            result.textContent = 'Round Tie!';
+            setTimeout(() => {
+                resultDialog.showModal();
+            }, 1000);
             console.log('Match Tie!');
-            return; 
+            return;
         }
     }
 
     return {
-        mark
+        mark, resetArray, p1Name, p2Name
     };
 
 })();
@@ -205,12 +236,12 @@ const gameBoard = (function () {
 function getIndices(whoseTurn, row, col, cellId) {
     console.log(whoseTurn);
     if (whoseTurn == 'Player 1') {
-      if(whoVersusWho == '1'){
-        gameBoard.mark(row, col, player1Choice, whoseTurn, player1Choice, player2Choice, cellId);
-      }
-      else{
-        gameBoard.mark(row, col, player1Choice, whoseTurn, player1Choice, computerChoice, cellId);
-      }
+        if (whoVersusWho == '1') {
+            gameBoard.mark(row, col, player1Choice, whoseTurn, player1Choice, player2Choice, cellId);
+        }
+        else {
+            gameBoard.mark(row, col, player1Choice, whoseTurn, player1Choice, computerChoice, cellId);
+        }
     }
     else if (whoseTurn == 'Player 2') {
         gameBoard.mark(row, col, player2Choice, whoseTurn, player1Choice, player2Choice, cellId);
@@ -221,88 +252,136 @@ function getIndices(whoseTurn, row, col, cellId) {
 }
 
 function getComputerIndices(whoseTurn) {
-      row = Math.floor(Math.random() * 3);
-      col = Math.floor(Math.random() * 3);
-      if(row == 0 && col == 0){
+    row = Math.floor(Math.random() * 3);
+    col = Math.floor(Math.random() * 3);
+    if (row == 0 && col == 0) {
         cellId = '0';
-      }
-      else if(row == 0 && col == 1){
+    }
+    else if (row == 0 && col == 1) {
         cellId = '1';
-      }
-      else if(row == 0 && col == 2){
+    }
+    else if (row == 0 && col == 2) {
         cellId = '2';
-      }
-      else if(row == 1 && col == 0){
+    }
+    else if (row == 1 && col == 0) {
         cellId = '3';
-      }
-      else if(row == 1 && col == 1){
+    }
+    else if (row == 1 && col == 1) {
         cellId = '4';
-      }
-      else if(row == 1 && col == 2){
+    }
+    else if (row == 1 && col == 2) {
         cellId = '5';
-      }
-      else if(row == 2 && col == 0){
+    }
+    else if (row == 2 && col == 0) {
         cellId = '6';
-      }
-      else if(row == 2 && col == 1){
+    }
+    else if (row == 2 && col == 1) {
         cellId = '7';
-      }
-      else if(row == 2 && col == 2){
+    }
+    else if (row == 2 && col == 2) {
         cellId = '8';
-      }
-      console.log(row,col,cellId)
-      gameBoard.mark(row, col, computerChoice, whoseTurn, player1Choice, computerChoice, cellId);
-  }
+    }
+    console.log(row, col, cellId)
+    gameBoard.mark(row, col, computerChoice, whoseTurn, player1Choice, computerChoice, cellId);
+}
 
-playBtns.forEach(btn =>{
-    btn.addEventListener('click',(event)=>{
-        whoVersusWho = event.target.dataset.id;
-        dialog.showModal();
-    })
+playWithPcbtn.addEventListener('click', (event) => {
+    whoVersusWho = event.target.dataset.id;
+    playerNameDialog.showModal();
+
+})
+playWithFrndbtn.addEventListener('click', (event) => {
+    whoVersusWho = event.target.dataset.id;
+    playersNameDialog.showModal();
+})
+playerNameSubmitBtn.addEventListener('click', (event) => {
+    event.preventDefault();
+    gameBoard.p1Name = playerName.value;
+    p1Status.textContent = playerName.value;
+    p2Status.textContent = 'Computer';
+    if (gameBoard.p1Name) {
+        playerNameDialog.close();
+        playWithPcbtn.style.display = 'none';
+        playWithFrndbtn.style.display = 'none';
+        board.style.display = "flex";
+        playerMarker.textContent = `${gameBoard.p1Name} Marker:`
+        markerDialog.showModal();
+    }
+})
+playersNameSubmitBtn.addEventListener('click', (event) => {
+    event.preventDefault();
+    gameBoard.p1Name = player1Name.value;
+    p1Status.textContent = player1Name.value;
+    gameBoard.p2Name = player2Name.value;
+    p2Status.textContent = player2Name.value;
+    gameBoard.p1Name = player1Name.value;
+    gameBoard.p2Name = player2Name.value;
+    if (gameBoard.p1Name && gameBoard.p2Name) {
+        playersNameDialog.close();
+        playWithPcbtn.style.display = 'none';
+        playWithFrndbtn.style.display = 'none';
+        board.style.display = "flex";
+        playerMarker.textContent = `${gameBoard.p1Name} Marker:`
+        markerDialog.showModal();
+    }
 })
 
-markerBtns.forEach(btn=>{
-    btn.addEventListener('click',(event)=>{
+markerBtns.forEach(btn => {
+    btn.addEventListener('click', (event) => {
         player1Choice = event.target.textContent;
-        dialog.close();
-        playBtns.forEach(btn=>{
-            btn.style.display = "none";
-        })
-        board.style.display = "grid";
+        markerDialog.close();
+        p1Marker.textContent = event.target.textContent;
+        p2Marker.textContent = (player1Choice == "X") ? "O" : "X";
+        player1Status.style.display = 'flex';
+        player2Status.style.display = 'flex';
+        restartBtn.style.display = 'block';
         if (whoVersusWho == '1') {
             player2Choice = (player1Choice == "X") ? "O" : "X";
-            window.alert(`Player 2 Marker is "${player2Choice}"`);
-            window.alert(`X plays first!`);
+            playFirstDialog.showModal();
             whoseTurn = (player1Choice == "X") ? 'Player 1' : 'Player 2';
             turn = true;
         }
         else {
             computerChoice = (player1Choice == "X") ? "O" : "X";
-            window.alert(`Computer Marker is "${computerChoice}"`);
-            window.alert(`X plays first!`);
+            playFirstDialog.showModal();
             whoseTurn = (player1Choice == "X") ? 'Player 1' : 'Computer';
-            if(whoseTurn == "Computer"){
-                getComputerIndices(whoseTurn);
+            if (whoseTurn == "Computer") {
+                setTimeout(() => {
+                    getComputerIndices(whoseTurn);
+                }, 1000);
             }
         }
     })
 })
-cells.forEach(cell =>{
-    cell.addEventListener('click',(event)=>{
+cells.forEach(cell => {
+    cell.addEventListener('click', (event) => {
         cellId = event.target.id;
         row = Number(event.target.dataset.row);
         col = Number(event.target.dataset.col);
-        if(whoseTurn == 'Computer' && whoVersusWho == '0'){
+        if (whoseTurn == 'Computer' && whoVersusWho == '0') {
             whoseTurn = 'Player 1';
         }
-        if(whoseTurn == 'Player 1' && whoVersusWho == '1' && (!turn)){
+        if (whoseTurn == 'Player 1' && whoVersusWho == '1' && (!turn)) {
             whoseTurn = 'Player 2';
         }
-        else if(whoseTurn == 'Player 2' && whoVersusWho == '1' && (!turn)){
+        else if (whoseTurn == 'Player 2' && whoVersusWho == '1' && (!turn)) {
             whoseTurn = 'Player 1';
         }
         console.log(whoseTurn);
-        getIndices(whoseTurn, row, col, cellId);
+        setTimeout(() => {
+            getIndices(whoseTurn, row, col, cellId);
+        }, 300);
         turn = false;
     })
 })
+restartBtn.addEventListener('click', () => {
+    gameBoard.resetArray();
+    resultDialog.close();
+})
+playAgainbBtn.addEventListener('click', () => {
+    window.location.reload();
+})
+
+function closeDialog() {
+    playFirstDialog.close();
+}
